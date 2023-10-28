fe437e7c8e1b0493d8d5f0188bcdaecc2175e515
ArkanZAG <arkanalghifari22@gmail.com>
2023-10-28 15:29:27 +0700

    Player Movement
    player can move yay

--- Assets/Scripts/Kelontong/Player/PlayerController.cs
+++ Assets/Scripts/Kelontong/Player/PlayerController.cs
@@ -10,8 +10,16 @@ namespace Kelontong.Player
 {
     public class PlayerController : MonoBehaviour
     {
+        [SerializeField] private float playerMovementSpeed;
+        [SerializeField] private Rigidbody playerRigidBody;
+        
+        
         private void Update()
         {
+            float horizontalInput = Input.GetAxis("Horizontal");
+            float verticalInput = Input.GetAxis("Vertical");
+            playerRigidBody.velocity = new Vector3(horizontalInput * playerMovementSpeed, 0, verticalInput * playerMovementSpeed);
+            
             if (Input.GetKeyDown(KeyCode.Space))
             {
                 
---
+++ Assets/Scripts/Kelontong/TEMP_ARYA/TestPlayerModuleLoader.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using Arr.ModulesSystem;
+using Kelontong.Modules;
+using UnityEngine;
+
+public class TestPlayerModuleLoader : ModulesLoader
+{
+    protected override BaseModule[] Modules => new[]
+    {
+        new PlayerSpawnerModule()
+    };
+
+}
