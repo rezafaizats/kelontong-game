1dd1346d9b9caeef76bd1b31f1f6a2d47447f5b5
ArkanZAG <arkanalghifari22@gmail.com>
2023-10-29 19:50:28 +0700

    Walk Back New Sprite

---
+++ Assets/Scripts/Kelontong/TEMP_ARYA/Test Script.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+    
+    
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            
+        }
+
+         
+    }
+}
